eef27936c0d5c93cea0926f1026d1516a4a374b8
BautyGarcia <bautistaroccogarcia@gmail.com>
2022-05-27 14:32:22 -0300

    Start

---
+++ Assets/Scripts/Player1Behaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Behaviour : MonoBehaviour
+{   
+    public float JumpForce;
+    Rigidbody rb;
+    bool hasJump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (hasJump){
+                rb.AddForce(Vector3.up * JumpForce,ForceMode.Impulse);
+                hasJump = false;
+            }
+        }
+        if (Input.GetKey(KeyCode.W)){
+            transform.Translate(0,0,-0.1f);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(0,0,0.1f);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            transform.Translate(0.1f,0,0);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            transform.Translate(-0.1f,0,0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Plataforma"){
+            hasJump = true;
+        }
+    }
+}
--- Assets/Scripts/Player2Behaviour.cs
+++ Assets/Scripts/Player2Behaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionBehaviour : MonoBehaviour
+public class Player2Behaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
